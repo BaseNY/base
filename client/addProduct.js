991388ecb383a686981fe612801dc68af136e569
Jasper Lu <repsajrules@gmail.com>
2013-12-17 20:59:32 -0800

    finished up the switching between stuff. need to add in meteor js to uload the final result into a dectionar

--- client/addProduct.js
+++ client/addProduct.js
@@ -1,27 +1,74 @@
 showStep = function() {
-    $('#product-info').children().css('display','none');
-    $('#product-info').children(':nth-child(' + Session.get('postState') + ')').css('display','block');
+    $('#product-info').children().removeClass('show');
+    $('#product-info').children(':nth-child(' + Session.get('postState') + ')').addClass('show');
+	    }
+
+verifyField = function(a) {
+    var b = a.find('input, select').val();
+    return (b != undefined && b != '') || Session.get('postState') == 5; 
 }
-/*
-Template.body.events({
-    'keydown': function(e) {
-	console.log(e);
-    }
-});
-*/
 
-Template.pageAddProduct.rendered = function(){
-    showStep();   
-    $(document).keypress(function(e) {
-	if(e.which == 13) {
-	}
-    });
-};
+	    Template.pageAddProduct.rendered = function(){
+		showStep();   
+		$('form').submit(function(e) {
+		    e.preventDefault();	    
+		});
+		$(document).keypress(function(e) {
+		    if(e.which == 13) {
+			$('.show .button').click();
+		    }
+		});
+	    };
 
-Template.pageAddProduct.events({
-    'click .button' : function() {
-	var s = Session.get('postState');
-	Session.set('postState',++s);
-	showStep();
-    }
+	    Template.pageAddProduct.events({
+		'click .button' : function() {
+		    var s = Session.get('postState');
+		    if(!verifyField($('.show'))) {
+			alert('You must fill out the field!');
+			return -1;
+		    }
+		    if(s == 5) {
+			var f = $('form').serializeArray();
+			var tempDict = {};
+			for (x in f) {
+			    tempDict[f[x].name] = f[x].value;
+			}
+			tempDict['description'] = $('#itemDescription').html();
+			Session.set('tempProdForm', tempDict);
+		    }
+		    Session.set('postState',++s);
+		    showStep();
+		},
+	    });
+
+Template.postPreview.helpers({
+    'title': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').title;
+    },
+    'category': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').category;
+//return $('[name=category]').val();
+    },
+    'so': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').so;
+//return $('[name=so]').val();
+    },
+    'bin': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').bin;
+//return $('[name=bin]').val();
+    },
+    'condition': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').condition;
+	//return $('[name=condition]').val();
+    },
+    'description': function() {
+	if(Session.get('tempProdForm'))
+	    return Session.get('tempProdForm').description;
+	//return $('#itemDescription').html();
+    },
 });
