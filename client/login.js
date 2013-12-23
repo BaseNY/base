00f157f20ef492ca8813b57048e282cb9092fdbb
Jasper Lu <repsajrules@gmail.com>
2013-12-23 14:25:15 -0800

    added facebook login stuff

--- client/login.js
+++ client/login.js
@@ -1,10 +1,22 @@
 Template.header.events({
     'click #login': function() {
-	Meteor.loginWithFacebook(function(e) {
+	Meteor.loginWithFacebook({
+	    requestPermissions:
+	    ['email',
+	    'user_about_me',
+	    'user_birthday',
+	    'user_location']
+	},function(e) {
 	    if(e) {console.log(e); alert(e)}
 	    else {
 		console.log("You have logged in!");
 	    }
 	})
+    }
+});
+
+Template.header.helpers({
+    'name': function() {
+	return Meteor.user().services.facebook.first_name;
     }
 });
--- server/server.js
+++ server/server.js
@@ -11,5 +11,19 @@ if (Meteor.isServer) {
 	    Items.insert(p);
 	    console.log(Items);
 	}
-    });
+    })
+    Meteor.startup(function () {
+        Accounts.loginServiceConfiguration.remove({
+            service: "facebook"
+        });
+        Accounts.loginServiceConfiguration.insert({
+
+
+            service: "facebook",
+            appId: "581722495207267",
+   //         appId:"1382563015306531",
+            secret:"c756e217e627b5699e96bfef0cfebede"
+ //           secret: "559eaba1052b2bf3e289d5111d1af729"
+        });
+    }); 
 }
