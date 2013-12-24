3e8aff384ed340703136713edf9da7ed736d628e
Steve Zhu <st.zhu1@gmail.com>
2013-12-24 12:20:25 -0500

    More path fixes

--- client/config.rb
+++ client/config.rb
@@ -1,12 +1,13 @@
 # Require any additional compass plugins here.
-require "susy"
+require 'susy'
 
 # Set this to the root of your project when deployed:
-http_path = ""
-css_dir = "/"
-sass_dir = "/"
-images_dir = ""
-javascripts_dir = ""
+http_path = 'client'
+css_dir = 'stylesheets'
+sass_dir = 'stylesheets'
+http_images_path = '../../images'
+javascripts_dir = ''
+output_style = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
