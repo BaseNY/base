81164cdc50c9d5636204cff59726e818f4b3adb0
Steve Zhu <st.zhu1@gmail.com>
2013-12-24 11:43:02 -0500

    Box sizing path fix

--- client/stylesheets/config.rb
+++ client/stylesheets/config.rb
@@ -2,9 +2,9 @@
 require "susy"
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "."
-sass_dir = "."
+http_path = ""
+css_dir = "/"
+sass_dir = "/"
 images_dir = ""
 javascripts_dir = ""
 
