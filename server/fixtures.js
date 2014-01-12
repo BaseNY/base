52431bd3eba0debb4d9aa99b2b5aeac77386aafa
Steve Zhu <st.zhu1@gmail.com>
2014-01-12 17:17:28 -0500

    Changed item styling and added more fields

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,21 +2,58 @@ if (Items.find().count() === 0) {
 	Items.insert({
 		name: 'Jesus Toaster',
 		seller: 'Jesus',
-		price: 10000
+		description: 'For making Jesus Toast',
+		pricing: {
+			list: 10000,
+			retail: 1000000
+		}
 	});
 	Items.insert({
-		name: 'Shoes',
-		seller: 'Amazon',
-		price: 42
+		name: 'Shoes'
+		,seller: 'Amazon'
+		,description: 'Shoes'
+		,pricing: {
+			list: 42
+		}
 	});
 	Items.insert({
-		name: 'Github',
-		seller: 'Github',
-		price: 19.99
+		name: 'Github'
+		,seller: 'Github'
+		,description: 'Git'
+		,pricing: {
+			list: 19.99
+		}
 	});
 	Items.insert({
-		name: 'Stuff',
-		seller: 'Somebody',
-		price: 5922
+		name: 'Stuff'
+		,seller: 'Somebody'
+		,description: 'Ton of stuff'
+		,pricing: {
+			list: 5922
+		}
+	});
+	Items.insert({
+		name: 'Stuff2'
+		,seller: 'Somebody'
+		,description: 'Ton of stuff'
+		,pricing: {
+			list: 500
+		}
+	});
+	Items.insert({
+		name: 'Stuff3'
+		,seller: 'Somebody'
+		,description: 'Ton of stuff'
+		,pricing: {
+			list: 101
+		}
+	});
+	Items.insert({
+		name: 'Stuff4'
+		,seller: 'Somebody'
+		,description: 'Ton of stuff'
+		,pricing: {
+			list: 3400
+		}
 	});
 }
