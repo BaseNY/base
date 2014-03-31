6a03c2425042b77d441f584bbe73f90c47d2f4bf
Steve Zhu <st.zhu1@gmail.com>
2014-03-31 12:38:34 -0400

    Search home

--- client/views/home.js
+++ client/views/home.js
@@ -1,6 +1,9 @@
 Template.home.events({
 	'submit #search-form': function(event) {
 		event.preventDefault();
-		
+		var $form = $(event.currentTarget).find('input');
+		var searchQuery = $form.val();
+		console.log("searched for: " + searchQuery);
+		searchItem("wot");
 	}
 });
