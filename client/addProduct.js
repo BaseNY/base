a5281d72c7046004c8464b44769cff87940306a9
Jasper Lu <repsajrules@gmail.com>
2014-04-11 01:30:28 -0700

    new front end for posting - straight on the home apge

--- client/addProduct.js
+++ client/addProduct.js
@@ -20,13 +20,13 @@ Template.addTitle.rendered = function(){
 
 
     $('form').submit(function(e) {
-	e.preventDefault();
+        e.preventDefault();
         var temp;
         if(Session.get('tempProdForm'))
-	    temp = Session.get('tempProdForm');
-	temp.title = $('input[name=title]').val();
-	Session.set('tempProdForm',temp);
-	Router.go('/sell/info');
+        temp = Session.get('tempProdForm');
+    temp.title = $('input[name=title]').val();
+    Session.set('tempProdForm',temp);
+    Router.go('/sell/info');
     });
 }
 
@@ -34,11 +34,11 @@ var populateInfo = function() {
     temp = Session.get('tempProdForm');
     $('input[name=title]').val(temp.title);
     $('input:radio[name="category"]').filter('[value=' + temp.category + ']').attr('checked', 'checked');
-/*
-    $('input[name=so]').val(temp.so);
-    $('input[name=bin]').val(temp.bin);
-    $('select[name=condition]').val(temp.condition);
-    */
+    /*
+       $('input[name=so]').val(temp.so);
+       $('input[name=bin]').val(temp.bin);
+       $('select[name=condition]').val(temp.condition);
+       */
     $('#description').html(temp.description);
 }
 
@@ -57,9 +57,9 @@ Template.addInfo.rendered = function() {
 
     //check if it should be here
     /*
-    if(!Session.get('tempProdForm'))
-        Router.go('/sell/title');
-        */
+       if(!Session.get('tempProdForm'))
+       Router.go('/sell/title');
+       */
 
     if(Session.get('tempProdForm')) {
         temp = Session.get('tempProdForm');
@@ -69,14 +69,14 @@ Template.addInfo.rendered = function() {
         e.preventDefault(); 
         temp = Session.get('tempProdForm'); 
         temp.title = $('input[name=title]').val();
-	temp.feeds = [];
+        temp.feeds = [];
         temp.feeds.push($('input[name=category]:checked').val());
         /*
-	temp.so = $('input[name=so]').val();
-	temp.bin = $('input[name=bin]').val();
-	temp.condition = $('select[name=condition]').val();
-        */
-	temp.description = $('#description').html();
+           temp.so = $('input[name=so]').val();
+           temp.bin = $('input[name=bin]').val();
+           temp.condition = $('select[name=condition]').val();
+           */
+        temp.description = $('#description').html();
         console.log(temp);
 
         //NEED A CHECK TO SEE IF THE FIELDS ARE ALL FILLED
@@ -97,62 +97,89 @@ Template.postPreview.rendered = function(){
     $($('.step-circle')[1]).addClass('complete');
     $($('.step-circle')[2]).addClass('active');
     $('#addProdBut').click(function(e) {
-	e.preventDefault();
-	console.log("adding item");
-	Meteor.call('addProduct', Session.get('tempProdForm'), function(e,r) {
-	    console.log("something");
-	    if(e) {
-		alert(e);
-	    }else{
-		console.log("done");
+        e.preventDefault();
+        console.log("adding item");
+        Meteor.call('addProduct', Session.get('tempProdForm'), function(e,r) {
+            console.log("something");
+            if(e) {
+                alert(e);
+            }else{
+                console.log("done");
                 Session.set('tempProdForm',null);
-		Router.go('/post/' + r);
-	    }
-	});
+                Router.go('/post/' + r);
+            }
+        });
     });
     $('#back').click(function(e) {
-	Router.go('/sell/info');
+        Router.go('/sell/info');
     });
 }
 
 Template.pageAddProduct.rendered = function(){
     if(!Session.get('tempProdForm'))
-	Session.set('tempProdForm',{});
+        Session.set('tempProdForm',{});
 };
 
 
 Template.postPreview.helpers({
     'title': function() {
-	if(Session.get('tempProdForm'))
+        if(Session.get('tempProdForm'))
     return Session.get('tempProdForm').title;
     },
     'category': function() {
-	if(Session.get('tempProdForm'))
+        if(Session.get('tempProdForm'))
     return Session.get('tempProdForm').category;
 //return $('[name=category]').val();
     },
     'so': function() {
-	if(Session.get('tempProdForm'))
+        if(Session.get('tempProdForm'))
     return Session.get('tempProdForm').so;
 //return $('[name=so]').val();
     },
     'bin': function() {
-	if(Session.get('tempProdForm'))
+        if(Session.get('tempProdForm'))
     return Session.get('tempProdForm').bin;
 //return $('[name=bin]').val();
     },
     'condition': function() {
-	if(Session.get('tempProdForm'))
-	    return Session.get('tempProdForm').condition;
-	//return $('[name=condition]').val();
+        if(Session.get('tempProdForm'))
+            return Session.get('tempProdForm').condition;
+        //return $('[name=condition]').val();
     },
     'description': function() {
-	if(Session.get('tempProdForm'))
-	    return Session.get('tempProdForm').description;
-	//return $('#itemDescription').html();
+        if(Session.get('tempProdForm'))
+            return Session.get('tempProdForm').description;
+        //return $('#itemDescription').html();
     },
 });
 
 Template.product.description = function() {
     return new Handlebars.SafeString(Router.current().data().item.description);
 }
+
+Template.postBox.feeds = function() {
+    return Feeds.find().fetch();
+}
+
+Template.postBox.rendered = function() {
+    $(document).ready(function() {
+        $('.highlight-radio[name=type]').change(function() {
+            $('.rad').removeClass('checked');
+            $('.highlight-label[for=' + this.id + ']').addClass('checked'); 
+        });
+        $('.cat').change(function() {
+            $('.cat').removeClass('checked');
+            $('.highlight-label[for=' + this.id + ']').addClass('checked');
+        });
+        
+    });
+}
+
+    /*
+       Template.postBox.events({
+       'change .highlight-radio' : function(e) {
+       console.log(e.target.id);
+       $('.highlight-label[for=' + e.target.id + ']').toggleClass('checked')
+       }
+       });
+       */
