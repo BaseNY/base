b3829928c3c0e395bef5a75336582c97a9a64b5e
Jasper Lu <repsajrules@gmail.com>
2014-04-11 04:46:16 -0700

    changed startup feeds colelction insert

--- server/server.js
+++ server/server.js
@@ -1,59 +1,59 @@
 if (Meteor.isServer) {
-            Feeds.remove({});
-            Feeds.insert({name:'Electronics'});
-            Feeds.insert({name:'Clothing'});
-            Feeds.insert({name:'Sneakers'});
+    if(Feeds.findOne() == undefined) {
+        Feeds.insert({name:'Electronics'});
+        Feeds.insert({name:'Clothing'});
+        Feeds.insert({name:'Sneakers'});
+    }
+    Meteor.methods({
+        addProduct: function(p) {
+            console.log('called');
+            p.sellerId = this.userId;
+            p.seller = Meteor.users.findOne({_id:Meteor.userId()}).profile.name;
+            p.time = new Date();
+            var temp = Items.insert(p);
+            console.log(temp);
+            //linker. pushes the id of the item, and the id of the category
+            for(x in p.feeds)
+        FtoI.insert({'feedId': p.feeds[x], 'itemId': temp});
+    return temp;
+        },
+    addBid: function(p) {
+        console.log('called' + p);
+        p.fromId = Meteor.userId();
+        p.from = Meteor.users.findOne({_id:Meteor.userId()}).profile.name;
+        p.time = new Date(); 
 
-	Meteor.methods({
-		addProduct: function(p) {
-			console.log('called');
-			p.sellerId = this.userId;
-                        p.seller = Meteor.users.findOne({_id:Meteor.userId()}).profile.name;
-			p.time = new Date();
-			var temp = Items.insert(p);
-                        console.log(temp);
-                        //linker. pushes the id of the item, and the id of the category
-                        for(x in p.feeds)
-                            FtoI.insert({'feedId': p.feeds[x], 'itemId': temp});
-			return temp;
-		},
-                addBid: function(p) {
-                    console.log('called' + p);
-                p.fromId = Meteor.userId();
-                    p.from = Meteor.users.findOne({_id:Meteor.userId()}).profile.name;
-                    p.time = new Date(); 
-                    
-                    var offer = {
-                        sellerId: p.toId,
-                        seller: p.to,
-                        buyerId: p.fromId,
-                        buyer: p.from,
-                        time: p.time,
-                        location: p.location,
-                        offer: p.offer,
-                        postId: p.postId
-                    };
+        var offer = {
+            sellerId: p.toId,
+            seller: p.to,
+            buyerId: p.fromId,
+            buyer: p.from,
+            time: p.time,
+            location: p.location,
+            offer: p.offer,
+            postId: p.postId
+        };
 
-                    var oId = Offers.insert(offer);
-                    p.offerId = oId;
+        var oId = Offers.insert(offer);
+        p.offerId = oId;
 
-                    /*
-                    if(p.buyer == p.seller) {
-                        return -1;
-                    }
-                    */
-                    return Messages.insert(p);
-                },
-                resetAccounts: function() {
-                    Meteor.users.remove({});
-                    console.log(Meteor.users.find().fetch());
-                },
-                resetItems: function() {
-                    Items.remove({});
-                },
-                resetMsg: function() {
-                    Messages.remove({});
-                    Offers.remove({}); 
-                },
-	})
+        /*
+           if(p.buyer == p.seller) {
+           return -1;
+           }
+           */
+        return Messages.insert(p);
+    },
+    resetAccounts: function() {
+        Meteor.users.remove({});
+        console.log(Meteor.users.find().fetch());
+    },
+    resetItems: function() {
+        Items.remove({});
+    },
+    resetMsg: function() {
+        Messages.remove({});
+        Offers.remove({}); 
+    },
+    })
 }
