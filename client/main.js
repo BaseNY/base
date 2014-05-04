e15d0e5965b5ede19c577e4e573f84911021d5a3
Jasper Lu <repsajrules@gmail.com>
2014-05-04 00:05:25 -0400

    autopublish removed

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
 Template.header.helpers({
     'imgUrl' : function() {
-        var u = Meteor.user().services.facebook;
-        url = u.img;
-        if(url == null)
-            url = 'http://graph.facebook.com/' + u.id + '/picture';
+        var url = Meteor.user().profile.img;
         return url;
-    }
+    },
+	'name': function() {
+    	return Meteor.user().profile.name;
+	}
 });
 
 Template.header.events({
