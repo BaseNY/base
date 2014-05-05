8be662b8efe7db937dbaf43d8fa6145fdd05f939
Steve Zhu <st.zhu1@gmail.com>
2014-05-05 18:50:41 -0400

    Main formatting

--- client/main.js
+++ client/main.js
@@ -3,24 +3,24 @@ UI.registerHelper('json', function(context) {
 });
 
 Template.header.helpers({
-    'imgUrl' : function() {
-        var url = Meteor.user().profile.img;
-        return url;
-    },
-	'name': function() {
-    	return Meteor.user().profile.name;
+	'imgUrl': function() {
+		var url = Meteor.user().profile.img;
+		return url;
 	},
-    'newMsgs': function() {
-        var num = Meteor.user().new_message;
-        if(num)
-            return num;
-        else
-            return '';
-    }
+	'name': function() {
+		return Meteor.user().profile.name;
+	},
+	'newMsgs': function() {
+		var num = Meteor.user().new_message;
+		if (num)
+			return num;
+		else
+			return '';
+	}
 });
 
 Template.header.events({
-    'click .fa-sign-out' : function() {
-        Meteor.logout();
-    },
+	'click .fa-sign-out': function() {
+		Meteor.logout();
+	},
 });
