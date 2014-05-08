4384f88d62bba8565bdd1fca373a7ad446936743
Jasper Lu <repsajrules@gmail.com>
2014-05-08 19:35:07 -0400

    removed unecessary server calls

--- server/publications.js
+++ server/publications.js
@@ -22,62 +22,6 @@ Meteor.publish('messages', function(selector, options) {
 	return Messages.find(selector);
 });
 
-Meteor.publish('ftoi', function(limit, id) {
-	if (id) {
-		if (id[0] == '@')
-			return FtoI.find({
-				sellerId: id.substring(1)
-			}, {
-				sort: {
-					score: -1
-				},
-				limit: limit
-			});
-		else
-			return FtoI.find({
-				feedId: id
-			}, {
-				sort: {
-					score: -1
-				},
-				limit: limit
-			});
-	} else
-		return FtoI.find({}, {
-			sort: {
-				score: -1
-			},
-			limit: limit
-		});
-});
-
-Meteor.publish('ftoItems', function(limit, id) {
-	filter = {};
-	if (id) {
-		if (id[0] == '@') {
-			filter = {sellerId: id.substring(1)};
-		} else {
-			filter = {feedId: id};
-		}
-	}
-
-	console.log(filter);
-
-	return Meteor.publishWithRelations({
-		handle: this,
-		collection: FtoI,
-		filter: filter,
-		options: {
-			sort: {score: -1},
-			limit: limit
-		},
-		mappings: [{
-			key: 'itemId',
-			collection: Items
-		}]
-	});
-});
-
 Meteor.publish('feeds', function() {
 	return Feeds.find();
 });
