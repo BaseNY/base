9d1f4ec133fa94ae9be9616abc27ce973dbbd62f
Steve Zhu <st.zhu1@gmail.com>
2014-05-10 13:47:27 -0400

    Removed unnecessary files in packages/

--- packages/packages.js
+++
@@ -1,7 +0,0 @@
-Npm.depends({
-        'elasticsearch': '2.1.4' // Where x.x.x is the version, e.g. 0.3.2
-});
-
-Package.on_use(function (api) {
-        api.add_files('elasticsearch.js', 'server'); // Or 'client', or ['server', 'client']
-});
