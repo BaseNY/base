4bb2fc96ecf4c711f24ad28ad2ee2bffa6e7a3ee
Steve Zhu <st.zhu1@gmail.com>
2014-05-25 21:07:13 -0400

    Put feed fixture in startup

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,28 +1,29 @@
-defaultFeedIds = [];
+Meteor.startup(function() {
+	defaultFeedIds = [];
+	defaultFeeds = [{
+		name: 'Sneakers',
+		icon: 'money'
+	}, {
+		name: 'Electronics',
+		icon: 'laptop'
+	}, {
+		name: 'Clothing (Men)',
+		icon: 'male'
+	}, {
+		name: 'Clothing (Women)',
+		icon: 'female'
+	}, {
+		name: 'Books',
+		icon: 'book'
+	}, {
+		name: 'Others',
+		icon: 'random'
+	}];
 
-defaultFeeds = [{
-	name: 'Sneakers',
-	icon: 'money'
-}, {
-	name: 'Electronics',
-	icon: 'laptop'
-}, {
-	name: 'Clothing (Men)',
-	icon: 'male'
-}, {
-	name: 'Clothing (Women)',
-	icon: 'female'
-}, {
-	name: 'Books',
-	icon: 'book'
-}, {
-	name: 'Others',
-	icon: 'random'
-}];
-
-if (!Feeds.findOne()) {
-	defaultFeedIds = _.map(defaultFeeds, function(feed) {
-		return Feeds.insert(feed);
-	});
-	console.log(defaultFeedIds);
-}
+	if (!Feeds.findOne()) {
+		defaultFeedIds = _.map(defaultFeeds, function(feed) {
+			return Feeds.insert(feed);
+		});
+		console.log(defaultFeedIds);
+	}
+});
