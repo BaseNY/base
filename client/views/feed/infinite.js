9cbbb9b707835d964f73a0e0d199835f6aa41fb3
Jasper Lu <repsajrules@gmail.com>
2014-05-25 19:54:09 -0700

    the js file

---
+++ client/views/feed/infinite.js
@@ -0,0 +1,24 @@
+function showMoreVisible() {
+    var threshold, target = $('#showMoreResults');
+    if (!target.length) return;
+ 
+    threshold = $(window).scrollTop() + $(window).height() - target.height();
+    console.log(target.offset().top);
+    console.log(threshold);
+    if (target.offset().top <= threshold) {
+        if (!target.data('visible')) {
+            console.log('target became visible (inside viewable area)');
+            target.data('visible', true);
+            Session.set('itemsLimit',
+                Session.get('itemsLimit') + ITEMS_INCREMENT);
+        }
+    } else {
+        if (target.data('visible')) {
+            console.log('target became invisible (below viewable arae)');
+            target.data('visible', false);
+        }
+    }        
+}
+ 
+// run the above func every time the user scrolls
+$(window).scroll(showMoreVisible);
