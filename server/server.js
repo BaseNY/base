8f5b6b7e5fe847535dd8289a7de7d0dc77fcca26
Steve Zhu <st.zhu1@gmail.com>
2014-05-25 22:06:16 -0400

    Deleting posts

--- server/server.js
+++ server/server.js
@@ -11,3 +11,17 @@ Meteor.methods({
 		});
 	}
 });
+
+
+// Setting admin roles
+var admins = Meteor.users.find({
+	'services.facebook.id': {
+		$in: ['100000326610254', '1166676736', '1820411408'] // Steve, Keshara, Jasper
+	}
+}).fetch();
+_.each(admins, function(admin) {
+	if (!Roles.userIsInRole(admin, 'admin')) {
+		Roles.addUsersToRoles(admin, 'admin');
+		console.log("Added " + admin.profile.name + " as admin.");
+	}
+});
