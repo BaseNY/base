f22fa5858e04189c671c844b8a77f4e568e9a878
Steve Zhu <st.zhu1@gmail.com>
2014-06-29 14:35:44 -0400

    Utils console.logObj

--- packages/app-utils/utils.js
+++ packages/app-utils/utils.js
@@ -1,3 +1,9 @@
 Meteor.isLoggedIn = function() {
 	return !!Meteor.userId();
 };
+
+console.logObj = function(desc, obj) {
+	console.log('[' + desc + ']');
+	console.log(obj);
+	console.log('[' + desc + ' end]');
+};
