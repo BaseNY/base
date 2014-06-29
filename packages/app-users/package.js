c72c16407bd6b16e7b56b986b69f31f5350a6b40
Steve Zhu <st.zhu1@gmail.com>
2014-06-29 19:28:42 -0400

    Moved users to package structure

---
+++ packages/app-users/package.js
@@ -0,0 +1,26 @@
+// regarding the fb friends updating
+// maybe we still should do a complete update every month or something
+// because it is possible that the site is down when fb sends the post request
+
+Package.describe({
+	summary: "Users, account creation, login"
+});
+
+Package.on_use(function(api) {
+	api.use([
+		'npm',
+
+		'accounts-base',
+		'service-configuration',
+
+		'simple-schema',
+
+		'app-fbgraph',
+		'app-utils',
+		'app-schemas',
+		'app-feed'
+	]);
+
+	api.add_files('js/config.js', 'server');
+	api.add_files('collections/users.js', 'server');
+});
