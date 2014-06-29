db32ff76a62582a92406aa234c9e2a4a54cdafd5
Steve Zhu <st.zhu1@gmail.com>
2014-06-29 10:54:39 -0400

    Cleaned up api.use and api.add_files

--- packages/app-messaging/package.js
+++ packages/app-messaging/package.js
@@ -3,27 +3,17 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-	api.use('underscore');
-
-	api.use('templating');
-	api.use('blaze-layout');
-	api.use('roles');
-	//api.use('iron-router');
-	//api.use('fast-render');
-
-	api.use('collection-hooks');
-
-	api.use('simple-schema');
-	api.use('collection2');
+	api.use(['underscore', 'templating', 'blaze-layout', 'roles']);
+	//api.use(['iron-router', 'fast-render']);
+	api.use(['collection-hooks', 'simple-schema', 'collection2']);
 	api.use('app-schemas');
 
-	api.add_files(['messaging.html'], 'client');
-
+	//api.add_files('js/controller.js', ['client', 'server']);
 	api.add_files([
 		'collections/conversations.js',
 		'collections/messages.js'
 	], ['client', 'server']);
-	//api.add_files('js/controller.js', ['client', 'server']);
+	api.add_files(['messaging.html'], 'client');
 	api.add_files('messaging.js', 'client');
 
 	api.export('Messages');
