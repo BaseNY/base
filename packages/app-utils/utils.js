dfe620ea33c1e54f02e3667b80aac2c919f975ce
Steve Zhu <st.zhu1@gmail.com>
2014-07-02 09:19:10 -0400

    Utils for generating a publish function

--- packages/app-utils/utils.js
+++ packages/app-utils/utils.js
@@ -7,3 +7,18 @@ console.logObj = function(desc, obj) {
 	console.log(obj);
 	console.log('[' + desc + ' end]');
 };
+
+Utils = {
+	// generates a publish function that simply allows selector and options pass through
+	generatePublishFunction: function(collection) {
+		return function(selector, options) {
+			if (!selector) {
+				selector = {};
+			}
+			if (!options) {
+				options = {};
+			}
+			return collection.find(selector, options);
+		};
+	}
+};
