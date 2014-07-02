a90f207abb25cf9055405a9229f603cd36ce3f1c
Steve Zhu <st.zhu1@gmail.com>
2014-07-02 12:01:18 -0400

    Utils

--- packages/app-utils/package.js
+++ packages/app-utils/package.js
@@ -3,6 +3,10 @@ Package.describe({
 });
 
 Package.on_use(function(api, where) {
+	api.use([
+		'templating'
+	]);
+
 	api.add_files('utils.js', ['client', 'server']);
 
 	api.export('Utils');
--- packages/app-utils/utils.js
+++ packages/app-utils/utils.js
@@ -22,3 +22,20 @@ Utils = {
 		};
 	}
 };
+
+if (Meteor.isClient) {
+	UI.helpers = function(helpers) {
+		_.each(helpers, function(func, name) {
+			UI.registerHelper(name, func);
+		});
+	};
+
+	UI.helpers({
+		'json': function(context) {
+			return JSON.stringify(context);
+		},
+		'profile': function() {
+			return Meteor.user().profile;
+		}
+	});
+}
