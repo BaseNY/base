7f937ddc57cd707b818a7be3d015088de545f55f
Jasper Lu <repsajrules@gmail.com>
2014-07-03 15:00:48 -0700

    post message

--- packages/app-messaging/views/messaging.js
+++ packages/app-messaging/views/messaging.js
@@ -2,6 +2,27 @@ var scrollDown = function(elem) {
 	elem.animate({scrollTop: elem[0].scrollHeight}, 50);
 };
 
+Template.messagingPost.messageSent = function(id) {
+    $('#message-'+this._id).parent().html('Your message has been sent! Click <a href="' + id + '">here</a> to view the conversation.');
+}
+
+Template.messagingPost.events({
+    'click .messageSend': function() {
+        item=Items.findOne({_id: this._id});
+
+		Messages.send($('#message-' + this._id).html(), item.sellerId, function(err, res) {
+			if (err) {
+				console.log(err);
+			} else {
+                
+                console.log(this);
+    $('#message-'+item._id).parent().html('Your message has been sent! Click <a href="/messages/' + res + '">here</a> to view the conversation.');
+
+			}
+		});
+    }
+});
+
 Template.messaging.events({
 	'keydown #messaging-reply': function(e) {
 		var $message = $("#messaging-reply"),
