2e7e34bdde49327a842a7b70674ff8fedc4629f6
Steve Zhu <st.zhu1@gmail.com>
2014-07-03 13:17:37 -0400

    Added responsive-modal package and .gitignore files for some packages

---
+++ packages/responsive-modal/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+	summary: "A responsive modal"
+});
+
+Package.on_use(function(api, where) {
+	api.use([
+		'lodash',
+		'jquery',
+
+		'bourbon',
+
+		'velocityjs'
+	]);
+	api.add_files([
+		'responsive-modal.js',
+	], 'client');
+});
