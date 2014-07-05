cc58fef22eebe948216ad7d4992b241b2902ab39
Steve Zhu <st.zhu1@gmail.com>
2014-07-05 11:37:43 -0400

    User config

--- packages/app-users/config.js
+++ packages/app-users/config.js
@@ -1,14 +1,18 @@
+Meteor._ensure(Meteor, 'settings', 'public');
+
 ServiceConfiguration.configurations.remove({
 	service: 'facebook'
 });
-ServiceConfiguration.configurations.insert({
-	service: 'facebook',
-	appId: "1423364661284723",
-	//appId: '199242350261474',
-    //jasper base2
-   // appId: '655164321229763',
-	secret: "940d8ca3eb8615602879739ade1201d4"
-	//secret: 'dcd13b60204fb657a47ca97da9e8daca'
-    //jasper base2
-    //secret: '8f0efa53e0417109a6d9f28226df89c0'
-});
+if (Meteor.settings.public.debug) {
+	ServiceConfiguration.configurations.insert({
+		service: 'facebook',
+		appId: "1423364661284723",
+		secret: "940d8ca3eb8615602879739ade1201d4"
+	});
+} else {
+	ServiceConfiguration.configurations.insert({
+		service: 'facebook',
+		appId: '655164321229763',
+		secret: '8f0efa53e0417109a6d9f28226df89c0'
+	});
+}
