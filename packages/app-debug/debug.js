94519b9df692b9a43d81aed779f7d6ca1bc772d6
Steve Zhu <st.zhu1@gmail.com>
2014-07-04 21:37:27 -0400

    More debug options

--- packages/app-debug/debug.js
+++ packages/app-debug/debug.js
@@ -3,27 +3,34 @@ Meteor._ensure(Meteor, 'settings', 'public');
 var debug = function(namespace) {
 	return function() {
 		if (Meteor.settings.public.debug) {
+			var desc, obj;
 			if (arguments.length === 2) {
-				var desc = arguments[0],
-					obj = arguments[1];
-				var c = '[';
-				if (namespace) {
-					c += namespace + ' - ';
-				}
-				c += desc;
-				console.log(c + ']');
-				console.log(obj);
-				console.log(c + ' end]');
+				desc = arguments[0];
+				obj = arguments[1];
 			} else {
-				console.log(arguments[0]);
+				obj = arguments[0];
 			}
+
+			var c = '[';
+			if (namespace) {
+				c += namespace + ' - ';
+			}
+			if (desc) {
+				c += desc;
+			}
+
+			console.log(c + ']');
+			console.log(obj);
+			console.log(c + ' end]');
 		}
 	}
 }
 
 // means debug for the package app-<key>
-var packages = ['feed', 'messaging', 'users']
-Debug = {};
+var packages = ['feed', 'messaging', 'users', 'utils'];
+Debug = {
+	log: debug()
+};
 _.each(packages, function(package) {
 	Debug[package] = debug('app-' + package);
 });
