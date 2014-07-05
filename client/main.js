814492f0f6f7d77e4bee6a1d95ccfa196f32a39c
Jasper Lu <repsajrules@gmail.com>
2014-07-05 13:12:07 -0700

    fixed some shit with the whole notifications

--- client/main.js
+++ client/main.js
@@ -1,82 +1,95 @@
-setPlaceholder = function($contenteditable) {
-	var placeholder = $contenteditable.attr('placeholder');
-	$contenteditable.html('<span class="placeholder">' + placeholder + '</span>');
+setPlaceholder = function($contenteditable) { var placeholder = $contenteditable.attr('placeholder');
+    $contenteditable.html('<span class="placeholder">' + placeholder + '</span>');
 };
 
 Template.layout.events({
-	'focus [contenteditable=true]': function(e) {
-		var $target = $(e.currentTarget);
-		if ($target.children('.placeholder').length !== 0) {
-			$target.empty();
-		}
-	},
-	'blur [contenteditable=true]': function(e) {
-		var $target = $(e.currentTarget);
-		if ($target.html().trim() === '') {
-			setPlaceholder($target);
-		}
-	}
+    'focus [contenteditable=true]': function(e) {
+        var $target = $(e.currentTarget);
+        if ($target.children('.placeholder').length !== 0) {
+            $target.empty();
+        }
+    },
+    'blur [contenteditable=true]': function(e) {
+        var $target = $(e.currentTarget);
+        if ($target.html().trim() === '') {
+            setPlaceholder($target);
+        }
+    }
 });
 
 Template.header.events({
-	'click #show-notifs': function() {
-		if (Session.get('showNotifs'))
-			Session.set('showNotifs', false);
-		else {
-			if (Notifications.find({
-				seen: false
-			}))
-				Meteor.call('glossedNotifs');
-			Session.set('showNotifs', true);
-		}
-	}
+    'click #show-notifs': function(e) {
+        if (Session.get('showNotifs'))
+    Session.set('showNotifs', false);
+        else {
+            if (Notifications.find({
+                seen: false
+            }))
+            Meteor.call('glossedNotifs');
+            Session.set('showNotifs', true);
+        }
+    }
 });
+
+Template.header.rendered = function(){
+    if(Session.get('showNotifs')) {
+        console.log('conflict?');
+        $("body").click(function(e) {
+            if (!(e.target.id == "notif-dropdown" || $(e.target).parents("#notif-dropdown").size())) { 
+                Session.set('showNotifs',false);
+            }
+        });
+    }
+};
 
 Template.header.helpers({
-	'newMsgs': function() {
-		var num = Meteor.user().new_message;
-		if (num)
-			return num;
-		else
-			return '';
-	},
-	'showNotifs': function() {
-		if (Session.get('showNotifs'))
-			return true;
-		else
-			return false;
-	},
-	'newNotifs': function() {
-		var num = Notifications.find({
-			seen: false
-		}).count();
-		if (num)
-			return num;
-		else
-			return '';
-	},
-	'notifs': function() {
-		return Notifications.find().fetch();
-	},
-	'unread': function() {
-		return !this.read;
-	},
-	'text': function() {
-		return formatNotif(this);
-	},
-	'notifImage': function() {
-		return Meteor.users.findOne({
-			_id: this.actorId
-		}).profile.img;
-	}
+    'newMsgs': function() {
+        var num = Meteor.user().new_message;
+        if (num)
+    return num;
+        else
+    return '';
+    },
+    'showNotifs': function() {
+        if (Session.get('showNotifs'))
+    return true;
+        else
+    return false;
+    },
+    'newNotifs': function() {
+        var num = Notifications.find({
+            seen: false
+        }).count();
+        if (num)
+    return num;
+        else
+            return '';
+    },
+    'notifs': function() {
+        return Notifications.find().fetch();
+    },
+});
+
+Template.notification.helpers({
+    'unread': function() {
+        return !this.read;
+    },
+    'text': function() {
+        return formatNotif(this);
+    },
+    'notifImage': function() {
+        return Meteor.users.findOne({
+            _id: this.actorId
+        }).profile.img;
+    }
 });
 
 Template.header.events({
-	'click .fa-sign-out': function() {
-		Meteor.logout();
-	},
-	'click .notif-link': function(e) {
-		Session.set('showNotifs', false);
-		Meteor.call('clearNotif', this._id);
-	}
+    'click .fa-sign-out': function() {
+        Meteor.logout();
+    },
+    'click .notif-link': function(e) {
+        Session.set('showNotifs', false);
+        Meteor.call('clearNotif', this._id);
+    }
 });
