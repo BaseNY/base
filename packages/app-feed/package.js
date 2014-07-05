8260e8492a238107a8a700d8ef8f48bdc8855ade
Steve Zhu <st.zhu1@gmail.com>
2014-07-04 22:05:08 -0400

    Moved feed js to views

--- packages/app-feed/package.js
+++ packages/app-feed/package.js
@@ -28,10 +28,10 @@ Package.on_use(function(api) {
 	api.add_files('fixtures.js', 'server');
 	api.add_files([
 		'views/feed.html',
-		'js/feed.js',
+		'views/feed.js',
 		'views/feed_post.html',
-		'js/feed_post.js',
-		'js/infinite.js'
+		'views/feed_post.js',
+		'views/infinite.js'
 	], 'client');
 	api.add_files('routing.js', ['client', 'server']);
 
