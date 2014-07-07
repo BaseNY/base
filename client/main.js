59a9315fb32edf922a8253aec33f224296bf61e4
Steve Zhu <st.zhu1@gmail.com>
2014-07-06 21:32:49 -0400

    Orange notifs

--- client/main.js
+++ client/main.js
@@ -16,10 +16,10 @@ Template.layout.events({
 Template.header.events({
 	'click #show-notifs': function(e) {
 		e.stopPropagation();
-		if (Session.get('showNotifs'))
+		if (Session.get('showNotifs')) {
 			Session.set('showNotifs', false);
-		else {
-			if (Notifications.find({seen: false})) {
+		} else {
+			if (Notifications.findOne({seen: false})) {
 				Meteor.call('glossedNotifs');
 			}
 			Session.set('showNotifs', true);
