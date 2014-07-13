ac895a7c41fde207c5dd29808fd04ddd9fb5ac33
Steve Zhu <st.zhu1@gmail.com>
2014-07-13 09:49:03 -0400

    Moved user publications to app-main

--- packages/app-main/publications.js
+++ packages/app-main/publications.js
@@ -1,3 +1,40 @@
+Meteor.publish('users', Utils.defaultPublishFunction(Meteor.users));
+
+/**
+ * Publishes data for the logged in user.
+ */
+Meteor.publish('userData', function() {
+	return Meteor.users.find({
+		_id: this.userId
+	}, {
+		fields: {
+			'_id': true,
+			'profile': true,
+			'createdAt': true,
+			'conversationIds': true,
+			'friendIds': true,
+			'subscribed': true,
+			'new_message': true,
+			'new': true,
+			'roles': true
+		}
+	});
+});
+
+// TODO change this so that only users that are needed are subscribed to
+Meteor.publish('allUserData', function() {
+	return Meteor.users.find({}, {
+		fields: {
+			'_id': true,
+			'profile': true,
+			'createdAt': true,
+			'friends': true,
+			'conversationIds': true
+		}
+	});
+});
+
+
 Meteor.publish('feeds', Utils.defaultPublishFunction(Feeds));
 
 Meteor.publish('notifs', function() {
