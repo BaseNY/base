27b1d092154ebed69426eb74c607a0cbbfa0889a
Steve Zhu <st.zhu1@gmail.com>
2014-07-17 10:58:05 -0400

    Fixed emailing

--- packages/app-email/email.js
+++ packages/app-email/email.js
@@ -1,29 +1,29 @@
 Debug.order('app-email/email.js');
 
 Meteor.startup(function() {
-	process.env.MAIL_URL = 'smtp://postmaster@base.us:32e2y27sqr75@smtp.mailgun.org:587';
+    process.env.MAIL_URL = 'smtp://postmaster@base.us:32e2y27sqr75@smtp.mailgun.org:587';
 });
 
 Meteor.methods({
-	'sendEmail': function(to, subject, text, html) {
-		return Email.send({
-			to: to,
-			from: 'Base <messages-noreply@base.us>',
-			subject: subject,
-			text: text,
-			html: html
-		});
-	}
+    'sendEmail': function(to, subject, text, html) {
+        return Email.send({
+            to: to,
+            from: 'Base <messages-noreply@base.us>',
+            subject: subject,
+            text: text,
+            html: html
+        });
+    }
 });
 
 Email.sendEmail = function(to, subject, text, html) {
-	return Email.send({
-		to: to,
-		from: 'Base <messages-noreply@base.us>',
-		subject: subject,
-		text: text,
-		html: html
-	});
+    return Email.send({
+        to: to,
+        from: 'Base <messages-noreply@base.us>',
+        subject: subject,
+        text: text,
+        html: html
+    });
 };
 
 /*
@@ -46,8 +46,13 @@ Email.sendEmail = function(to, subject, text, html) {
 */
 
 Email.sendWelcomeEmail = function(user) {
-	// template in email.handlebars
-	return Email.sendEmail(user.profile.email, Handlebars.templates.email({
-		user: user
-	}));
+    // template in email.handlebars
+    return Email.sendEmail(
+        user.profile.email,
+        'Welcome to Base',
+        'You are now officially part of a growing community of buyers and sellers just like you',
+        Handlebars.templates.email({
+            user: user
+        })
+    );
 };
