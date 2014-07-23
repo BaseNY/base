c1d32bc6b5fb22efd340537187670e0de7aa0ee2
Steve Zhu <st.zhu1@gmail.com>
2014-07-23 15:01:07 -0400

    Offers tests

--- packages/app-collections/tests/collections/offers_test.js
+++ packages/app-collections/tests/collections/offers_test.js
@@ -1,13 +1,39 @@
+if (Meteor.isServer) {
+	Meteor.methods({
+		clearOffers: function() {
+			Offers.remove();
+		}
+	});
+}
+
 if (Meteor.isClient) {
 	var post;
+	var createdOfferId;
 	Tinytest.addAsync("Offers - Insert - Create offer", function(test, next) {
-		Meteor.subscribe('posts', function(err) {
-			post = Posts.findOne();
-			Offers.create(post, function(err, _id) {
-				test.isUndefined(err, "Expected no error to occur: " + err);
-				next();
+		Meteor.call('clearOffers', function(err, res) {
+			Meteor.subscribe('posts', function(err) {
+				post = Posts.findOne();
+				Offers.create(post, function(err, _id) {
+					test.isUndefined(err, "Expected no error to occur: " + err);
+					createdOfferId = _id;
+					next();
+				});
 			});
-		})
+		});
+	});
+
+	Tinytest.addAsync("Offers - Insert - Create offer that is identical to the previous", function(test, next) {
+		Offers.create(post, function(err, _id) {
+			test.isTrue(err instanceof Error, "Expected error to occur because you shouldn't be able to create two offers with the same post and buyer: " + err);
+			next();
+		});
+	});
+
+	Tinytest.addAsync("Offers - Remove - Remove offer", function(test, next) {
+		Offers.remove(createdOfferId, function(err) {
+			test.isUndefined(err, "Expected no error to occur: " + err);
+			next();
+		});
 	});
 }
 
