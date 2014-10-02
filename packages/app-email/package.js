a46f9065d550856b234b61dc71ed31bd2bd0a580
Steve Zhu <st.zhu1@gmail.com>
2014-10-02 17:48:48 -0400

    app-email

---
+++ packages/app-email/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+	summary: "Email",
+	version: "0.1.0"
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('METEOR@0.9.3.1');
+
+	api.use([
+		'email',
+
+		'cmather:handlebars-server@2.0.0',
+
+		'app-debug'
+	]);
+	api.add_files([
+		'email.handlebars',
+		'email.js'
+	], 'server');
+	api.export('Email');
+});
